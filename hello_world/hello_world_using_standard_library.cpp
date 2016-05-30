79daefe130a8744fe1b945753cb3ed7fd0aa02a5
Saydul Bashar <tamimnoone@gmail.com>
2016-05-30 14:51:47 +0100

    Added another file where hello world uses namespace std and standard library string class.

---
+++ hello_world/hello_world_using_standard_library.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string s1 = "Hello,";
+    string s2 = "World!";
+
+    cout << s1 + " " + s2 << endl;
+
+    return 0;
+}
